bb8689d4c4fea71e3c76093dcb893af2795bfa92
Ali <ali.azadi.dev@gmail.com>
2026-07-23 15:36:37 +0330

    feat(InputLogin): add custom value,onchane  prop

--- src/Components/Login/InputLogin.jsx
+++ src/Components/Login/InputLogin.jsx
@@ -1,8 +1,10 @@
-const InputLogin = ({ type, placeholder, className = "" }) => {
+const InputLogin = ({ type, value, placeholder, onChange, className = "" }) => {
   return (
     <input
       type={type}
+      value={value}
       placeholder={placeholder}
+      onChange={onChange}
       className={`w-full border-2 border-My-gray rounded-2xl py-3 px-8 ${className}`}
     />
   );
--- src/Pages/Forgotpassword.jsx
+++ src/Pages/Forgotpassword.jsx
@@ -5,8 +5,8 @@ const Forgotpassword = () => {
   return (
     <div className=" w-full h-screen bg-My-blue flex flex-col items-center justify-center gap-7 p-3">
       <h3 className=" flex items-center text-white text-[24px] gap-2">
-        <IoIosArrowBack />
         فراموشی رمز عبور
+        <IoIosArrowBack />
       </h3>
       <div className="flex-col items-center justify-start w-full max-w-100   bg-white rounded-[20px] p-4">
         <div className="w-full flex flex-col items-center justify-center  p-3 gap-5">
@@ -14,7 +14,6 @@ const Forgotpassword = () => {
            لطفا کد ملی یا شماره دانشجویی خود را وارد نمایید
           </h5>
           <InputLogin type="text" placeholder="کد ملی یا شماره دانشجویی" />
-
 
           <button className="w-full border-2 border-My-gray bg-My-purlpe text-white  rounded-2xl p-3">
             ارسال پیامک
